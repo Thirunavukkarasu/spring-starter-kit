8004a23d9aa2b2b19ded1494d0f76a05602d7d1c
Thirunavukkarasu <thirunavukkarasu.m@getvymo.com>
2022-09-20 14:59:24 +0530

    added screenshots

---
+++ spring-cloud-sleuth/Readme.md
@@ -0,0 +1,11 @@
+# Spring Cloud Sleuth 
+
+You can see in the below screenshot while hitting the API endpoint `/healthcheck` the log is getting printed with TraceID and default SpanID as well.
+
+## REST API Endpoint 
+
+![Spring Cloud Sleuth API Demo](screenshots/api.png "Log with TraceID and SpanID")
+
+## Application Logs
+
+![Spring Cloud Sleuth Demo](screenshots/demo.png "Log with TraceID and SpanID")
--- spring-cloud-sleuth/src/main/java/com/sb/controller/SleuthController.java
+++ spring-cloud-sleuth/src/main/java/com/sb/controller/SleuthController.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class SleuthController {
 
 	public static Logger logger = LoggerFactory.getLogger(SleuthController.class);
-	@GetMapping(value="/healthcheck")
+
+	@GetMapping(value = "/healthcheck")
 	public String getHealthcheck() {
 		logger.info("Entering Healthcheck!");
-		String message = "Hello Sleuth!";
+		String responseContent = "<html>\n" + "<header><title>Welcome</title></header>\n" +
+				"<body><h1>\n" + "Hello Sleuth!\n"
+				+ "</h1><p>Spring Cloud Sleuth allows you to aggregate and track log entries as requests move through a distributed software system. In a monolithic system, it’s relatively easy to track requests as they move through the codebase because all requests can easily be logged to the same log file. You can generally just filter the log by the thread ID. But in a distributed system, a single client request may sprawl across any number of discrete cloud services. Any given service may have multiple instances handling different parts of the request. There is no single log file, with a request spread across multiple server instances. How do you use logs in this situation? How do you trace a request flow across a service mesh?</p></body>\n"
+				+ "</html>";
 		logger.info("Leaving Healthcheck!");
-		return message;
+		return responseContent;
 	}
 }
