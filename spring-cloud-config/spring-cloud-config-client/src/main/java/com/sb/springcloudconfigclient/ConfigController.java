60f22f79ffbec02f3418bb63697d07e7967b8d2e
Thirunavukkarasu <thirunavukkarasu.m@getvymo.com>
2022-09-21 13:22:19 +0530

    added config client and server

---
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/com/sb/springcloudconfigclient/ConfigController.java
@@ -0,0 +1,27 @@
+package com.sb.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+class ConfigController {
+
+    @Value("${user.role}")
+    private String role;
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    @GetMapping(value = "/whoami/{username}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
+    }
+}
---
+++ spring-cloud-config/spring-cloud-config-server/src/test/java/com/sb/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
